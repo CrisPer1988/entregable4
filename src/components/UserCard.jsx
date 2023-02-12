c596b72e259257bbbe221c47d4f576b08a8aa91c
Alejandro Aguilar <almedina1601@gmail.com>
2023-02-12 09:41:09 -0800

    Iconos de email, birthday agregados, efecto hover en botones de delete & edit user

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const UserCard = ({user, deleteUserById, setUpdateInfo, handleOpen}) => {
-    const handleDelete = () => {
-        deleteUserById(user.id)
-    }
+const UserCard = ({ user, deleteUserById, setUpdateInfo, handleOpen }) => {
+  const handleDelete = () => {
+    deleteUserById(user.id);
+  };
 
-    const handleUpdate = () =>{
-      setUpdateInfo(user)
-      handleOpen()
-    }
+  const handleUpdate = () => {
+    setUpdateInfo(user);
+    handleOpen();
+  };
 
   return (
-    <article className='card__container'>
-        <h2 className='card__info'>{`${user.first_name} ${user.last_name}`}</h2>
-        <ul className='ul__list'>
-            <li className='card__list'><span>Email: </span>{user.email}</li>
-            <li className='card__list'><span>Birthday: </span>{user.birthday}</li>
-        </ul>
-        <div className='container__btn-card'>
-          <button className='card__btn card__btn-delete' onClick={handleDelete}><i class='bx bxs-trash-alt'></i></button>
-          <button onClick={handleUpdate} className='card__btn card__btn-edit'><i class='bx bx-edit-alt' ></i></button>
-        </div>
+    <article className="card__container">
+      <h2 className="card__info">{`${user.first_name} ${user.last_name}`}</h2>
+      <ul className="ul__list">
+        <li className="card__list">
+          <i class="bx bxs-envelope card__icon"></i>:{user.email}
+        </li>
+        <li className="card__list">
+          <i class="bx bxs-cake card__icon"></i>:{user.birthday}
+        </li>
+      </ul>
+      <div className="container__btn-card">
+        <button className="card__btn card__btn-delete" onClick={handleDelete}>
+          <i class="bx bxs-trash-alt card__icon--opt"></i>
+        </button>
+        <button onClick={handleUpdate} className="card__btn card__btn-edit">
+          <i class="bx bx-edit-alt card__icon--opt"></i>
+        </button>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default UserCard
+export default UserCard;
