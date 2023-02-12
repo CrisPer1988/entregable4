4fc925d8d4e78538c383ebf3c4b86ee8151123e1
Alejandro Aguilar <almedina1601@gmail.com>
2023-02-12 10:54:54 -0800

    Footer & demas iconos

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -15,18 +15,18 @@ const UserCard = ({ user, deleteUserById, setUpdateInfo, handleOpen }) => {
       <h2 className="card__info">{`${user.first_name} ${user.last_name}`}</h2>
       <ul className="ul__list">
         <li className="card__list">
-          <i class="bx bxs-envelope card__icon"></i>:{user.email}
+          <i className="bx bxs-envelope card__icon"></i>:{user.email}
         </li>
         <li className="card__list">
-          <i class="bx bxs-cake card__icon"></i>:{user.birthday}
+          <i className="bx bxs-cake card__icon"></i>:{user.birthday}
         </li>
       </ul>
       <div className="container__btn-card">
         <button className="card__btn card__btn-delete" onClick={handleDelete}>
-          <i class="bx bxs-trash-alt card__icon--opt"></i>
+          <i className="bx bxs-trash-alt card__icon--opt"></i>
         </button>
         <button onClick={handleUpdate} className="card__btn card__btn-edit">
-          <i class="bx bx-edit-alt card__icon--opt"></i>
+          <i className="bx bx-edit-alt card__icon--opt"></i>
         </button>
       </div>
     </article>
